1e1605b696f1f57294c4f6b2b5d893890ea6a424
Paulo Mulotto <psegitt2@gmail.com>
2022-12-13 11:27:18 -0300

    feat(valor_ativos): Somar o valor total dos ativos
    Adicionar o método na classe Estoque para calcular o valor total dos
    ativos no estoque da loja.

--- estoque.py
+++ estoque.py
@@ -2,3 +2,6 @@ class Estoque:
     def __init__(self, loja):
         self.produtos = []
         self.loja = loja
+
+    def calcula_valor_ativos(self):
+        sum(produto.valor_custo for produto in self.produtos)
