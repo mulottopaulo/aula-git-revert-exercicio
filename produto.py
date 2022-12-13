e917a06ee12af6da38f3e6b80b357760658afbef
Paulo Mulotto <psegitt2@gmail.com>
2022-12-13 11:00:32 -0300

    Criação de Loja e Produto

---
+++ produto.py
@@ -0,0 +1,5 @@
+class Produto:
+    def __init__(self, nome, valor_custo, valor_venda, loja):
+        self.nome = nome
+        self.valor_custo = valor_custo
+        self.valor_venda = valor_venda
